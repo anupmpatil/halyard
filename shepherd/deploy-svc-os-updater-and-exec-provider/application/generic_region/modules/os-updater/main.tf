b59d1da53b0882f4f2e47cac877aad716479682e
Raj Jumani <raj.jumani@oracle.com>
2021-01-12 20:44:47 +0000

    os updater bug fix

--- shepherd/deploy-svc-os-updater-and-exec-provider/application/generic_region/modules/os-updater/main.tf
+++ shepherd/deploy-svc-os-updater-and-exec-provider/application/generic_region/modules/os-updater/main.tf
@@ -1,8 +1,9 @@
 resource "odo_deployment" "os-updater-management-plane" {
-  for_each = toset(var.availability_domains)
-  ad       = each.value
-  alias    = var.odo_app_alias_mp
-  flags    = ["SKIP_UP_TO_DATE_NODES"]
+  for_each   = toset(var.availability_domains)
+  ad         = each.value
+  alias      = var.odo_app_alias_mp
+  is_overlay = true
+  flags      = ["SKIP_UP_TO_DATE_NODES"]
   artifact {
     url       = var.artifact_versions[var.artifact_name].uri
     build_tag = var.artifact_versions[var.artifact_name].version
@@ -12,10 +13,11 @@ resource "odo_deployment" "os-updater-management-plane" {
 }
 
 resource "odo_deployment" "os-updater-control-plane" {
-  for_each = toset(var.availability_domains)
-  ad       = each.value
-  alias    = var.odo_app_alias_cp
-  flags    = ["SKIP_UP_TO_DATE_NODES"]
+  for_each   = toset(var.availability_domains)
+  ad         = each.value
+  alias      = var.odo_app_alias_cp
+  is_overlay = true
+  flags      = ["SKIP_UP_TO_DATE_NODES"]
   artifact {
     url       = var.artifact_versions[var.artifact_name].uri
     build_tag = var.artifact_versions[var.artifact_name].version
@@ -25,9 +27,10 @@ resource "odo_deployment" "os-updater-control-plane" {
 }
 
 resource "odo_deployment" "os-updater-bastion" {
-  ad    = var.availability_domains[0]
-  alias = var.odo_app_alias_bastion
-  flags = ["SKIP_UP_TO_DATE_NODES"]
+  ad         = var.availability_domains[0]
+  alias      = var.odo_app_alias_bastion
+  is_overlay = true
+  flags      = ["SKIP_UP_TO_DATE_NODES"]
   artifact {
     url       = var.artifact_versions[var.artifact_name].uri
     build_tag = var.artifact_versions[var.artifact_name].version
