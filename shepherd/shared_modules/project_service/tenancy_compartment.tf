0147b21169ea17477a330dafb01e7a4053cbc06c
Amy Kang <amy.kang@oracle.com>
2021-03-05 05:05:54 +0000

    Merge pull request #87 in DLCDEP/deployment-service-config from project-ocid to master

--- shepherd/shared_modules/project_service/tenancy_compartment.tf
+++ shepherd/shared_modules/project_service/tenancy_compartment.tf
@@ -1,27 +1,22 @@
 locals {
-  project_svc_tenancy_name_map = {
-    "beta"    = "dlcproject"
-    "preprod" = "devopsprojectpre"
-    "prod"    = "devopsproject"
+  project_svc_tenancy_ocid_map = {
+    "beta" = "ocid1.tenancy.oc1..aaaaaaaahsav5slbvffyyakyjy7ei3g75wlyg65zbahrmn2hrkrc6rrhqqxa" //dlcproject
+
+    "preprod" = "ocid1.tenancy.oc1..aaaaaaaaxk63qg5pdoarb5ujeciq2rqjun6n23tszzwofrqaq7thiaoz7eja" //devopsprojectpre
+
+    "prod" = "ocid1.tenancy.oc1..aaaaaaaaiw6ql6qiyfaed2ug3escrjar5azwwkf73efwmwpr4jryirourifq" //devopsproject
   }
 
   project_svc_cp_compartment_name = "project_service_control_plane_api"
 
-  project_svc_tenancy_id = data.tenancylookup_ocid.project_svc_tenancy.id
+  project_svc_tenancy_id = lookup(local.project_svc_tenancy_ocid_map, var.environment, "not_defined")
 
-  project_svc_cp_compartment_id = data.oci_identity_compartments.project_svc_compartment.compartments[0].id
+  project_svc_cp_compartment_id = [for c in data.oci_identity_compartments.all_compartments.compartments : c if c.name == local.project_svc_cp_compartment_name][0]
+
 }
 
-data "tenancylookup_ocid" "project_svc_tenancy" {
-  name = lookup(local.project_svc_tenancy_name_map, var.environment, "not_defined")
-}
-
-data "oci_identity_compartments" "project_svc_compartment" {
+data "oci_identity_compartments" "all_compartments" {
   compartment_id = local.project_svc_tenancy_id
-  filter {
-    name   = "name"
-    values = [local.project_svc_cp_compartment_name]
-  }
 }
 
 output "project_svc_cp_tenancy_id" {
@@ -31,3 +26,4 @@ output "project_svc_cp_tenancy_id" {
 output "project_svc_cp_compartment_id" {
   value = local.project_svc_cp_compartment_id
 }
+
